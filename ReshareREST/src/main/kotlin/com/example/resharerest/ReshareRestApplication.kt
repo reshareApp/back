68109f6d1ef110f8850b7ed0be5fab72733ca540
OmarGomaaFCi <omargomaafcitu@gmail.com>
2024-01-29 04:27:56 +0200

    Add bootstrap project with base dependencies

---
+++ ReshareREST/src/main/kotlin/com/example/resharerest/ReshareRestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.resharerest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReshareRestApplication
+
+fun main(args: Array<String>) {
+    runApplication<ReshareRestApplication>(*args)
+}
