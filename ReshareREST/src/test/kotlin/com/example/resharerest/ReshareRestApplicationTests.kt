3902e1f6d9468c3bfb874811189a37dd6c0af19b
OmarGomaaFCi <omargomaafcitu@gmail.com>
2024-01-29 04:27:56 +0200

    Add bootstrap project with base dependencies

---
+++ ReshareREST/src/test/kotlin/com/example/resharerest/ReshareRestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.resharerest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReshareRestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
