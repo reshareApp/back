111e3b1f87ad302d168edef790045a5a017a4eff
OmarGomaaFCi <omargomaafcitu@gmail.com>
2024-01-31 23:09:44 +0200

    Implement `UserDetailsService` by using UserRepository

--- src/main/kotlin/com/re/back/configurations/ApplicationConfiguration.kt
+++ src/main/kotlin/com/re/back/configurations/ApplicationConfiguration.kt
@@ -1,9 +1,22 @@
 package com.re.back.configurations
 
+import com.re.back.repositories.AppUsersRepository
+import com.re.back.repositories.getResult
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.core.userdetails.UserDetailsService
 
 @Configuration
 @EnableConfigurationProperties(JwtProperties::class)
-class ApplicationConfiguration {
+class ApplicationConfiguration(private val usersRepository: AppUsersRepository) {
+
+    @Bean
+    fun userDetailsService(): UserDetailsService? {
+        return UserDetailsService { userName: String ->
+            usersRepository
+                .findByUserName(userName)
+                .orElse(null)
+        }
+    }
 }
