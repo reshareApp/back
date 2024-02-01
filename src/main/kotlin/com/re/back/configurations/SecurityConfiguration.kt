ef34f13716e3b1c46db0c1e20a941a29b8cf32f3
OmarGomaaFCi <omargomaafcitu@gmail.com>
2024-02-01 18:23:41 +0200

    add SecurityConfiguration

---
+++ src/main/kotlin/com/re/back/configurations/SecurityConfiguration.kt
@@ -0,0 +1,73 @@
+package com.re.back.configurations
+
+import com.re.back.configurations.entrypoints.DelegatedAuthenticationEntryPoint
+import com.re.back.security.filters.JwtAuthenticationFilter
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfiguration(
+    val jwtAuthenticationFilter: JwtAuthenticationFilter,
+    val authenticationEntryPoint: DelegatedAuthenticationEntryPoint,
+    val userDetailsService: UserDetailsService
+) {
+
+    @Bean
+    @Throws(java.lang.Exception::class)
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .csrf(CsrfConfigurer<HttpSecurity>::disable)
+            .authorizeHttpRequests { request ->
+                request.requestMatchers(
+                    "/api/v*/auth/**",
+                    "/v3/api-docs/**",
+                    "/swagger-ui/**",
+                    "/api/v*/images/**"
+                ).permitAll()
+            }
+            .sessionManagement { manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .authenticationProvider(usersAuthenticationProvider())
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
+            .exceptionHandling { c -> c.authenticationEntryPoint(authenticationEntryPoint) }
+
+        return http.build()
+    }
+
+    @Bean
+    fun bCryptPasswordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+
+    @Bean
+    fun usersAuthenticationProvider(): AuthenticationProvider {
+        val authProvider = DaoAuthenticationProvider()
+        authProvider.setUserDetailsService(userDetailsService)
+        authProvider.setPasswordEncoder(bCryptPasswordEncoder())
+        return authProvider
+    }
+
+    @Bean
+    @Throws(Exception::class)
+    fun authenticationManager(http: HttpSecurity): AuthenticationManager {
+        val authenticationManagerBuilder = http.getSharedObject(
+            AuthenticationManagerBuilder::class.java
+        )
+        authenticationManagerBuilder.authenticationProvider(usersAuthenticationProvider())
+        return authenticationManagerBuilder.build()
+    }
+
+}
