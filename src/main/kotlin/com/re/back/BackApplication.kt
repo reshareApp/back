8f447696b2b0ee54f7d2a7ae924920be4f24378f
OmarGomaaFCi <omargomaafcitu@gmail.com>
2024-02-01 18:21:40 +0200

    add SwaggerConfiguration

--- src/main/kotlin/com/re/back/BackApplication.kt
+++ src/main/kotlin/com/re/back/BackApplication.kt
@@ -1,9 +1,21 @@
 package com.re.back
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.servers.Server
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.CrossOrigin
 
 @SpringBootApplication
+@OpenAPIDefinition(
+    servers = [
+        Server(
+            url = "http://localhost:9898",
+            description = "Local Project for ReShare"
+        )
+    ]
+)
+@CrossOrigin(origins = ["*"], maxAge = 3600, allowedHeaders = ["*"])
 class BackApplication
 
 fun main(args: Array<String>) {
