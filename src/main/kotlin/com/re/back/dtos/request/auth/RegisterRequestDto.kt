b7372cc82553f61e7cb283222d539e74fbdfabb9
OmarGomaaFCi <omargomaafcitu@gmail.com>
2024-02-03 01:14:50 +0200

    remove unnecessary fields validation

--- src/main/kotlin/com/re/back/dtos/request/auth/RegisterRequestDto.kt
+++ src/main/kotlin/com/re/back/dtos/request/auth/RegisterRequestDto.kt
@@ -7,19 +7,13 @@ data class RegisterRequestDto(
     @field:Email(
         message = "Invalid email format. Please provide a valid email address."
     )
-    @field:NotNull
     @field:NotBlank
-    @field:NotEmpty
     val email: String,
 
-    @field:NotNull
     @field:NotBlank
-    @field:NotEmpty
     val userName: String,
 
-    @field:NotNull
     @field:NotBlank
-    @field:NotEmpty
     @field:Pattern(
         regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@\$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
         message = "Password must be at least 8 characters long and contain at least one lowercase letter, one uppercase letter, one digit, and one special character."
