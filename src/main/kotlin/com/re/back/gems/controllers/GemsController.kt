e9bf43830ee63e55710fd39ac5bebc339299459f
OmarGomaaFCi <omargomaafcitu@gmail.com>
2024-02-20 22:53:57 +0200

    add `GemsController` with `POST` add gem endpoint

---
+++ src/main/kotlin/com/re/back/gems/controllers/GemsController.kt
@@ -0,0 +1,21 @@
+package com.re.back.gems.controllers
+
+import com.re.back.extensions.buildOkApiResponseEntity
+import com.re.back.gems.GemsService
+import com.re.back.gems.dtos.request.GemRequestDto
+import jakarta.validation.Valid
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+@RequestMapping("\${api.version}/gems")
+class GemsController(private val gemsService: GemsService) {
+
+
+    @PostMapping
+    fun addGem(@RequestBody @Valid gemRequestDto: GemRequestDto) =
+        gemsService.addGem(gemRequestDto).buildOkApiResponseEntity()
+}
