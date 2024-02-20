57f5cfbec4eda127659d808dcd9ae09099162f9a
OmarGomaaFCi <omargomaafcitu@gmail.com>
2024-02-20 22:52:11 +0200

    add needed repositories queries

--- src/main/kotlin/com/re/back/gems/repositories/TagsRepository.kt
+++ src/main/kotlin/com/re/back/gems/repositories/TagsRepository.kt
@@ -3,6 +3,9 @@ package com.re.back.gems.repositories;
 import com.re.back.gems.entities.Tag
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.Optional
 
 @Repository
-interface TagsRepository : JpaRepository<Tag, Int>
+interface TagsRepository : JpaRepository<Tag, Int> {
+    fun findByName(name : String) : Optional<Tag>
+}
